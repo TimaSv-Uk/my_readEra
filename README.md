8d101e54a94d03868c54324d5f80fb2dab1be23d
TimaSv-Uk <sviridenkotimofei@gmail.com>
2025-12-13 15:22:15 +0200

    setup astro

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # my_readEra
-display of all my have_read books in html/js
+
+display of all my have_read books in html/js 
+
+data is taken  from ReadEra(android App) back up file 
+
---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+
+export default defineConfig({
+  site: "https://timasv-uk..github.io",
+  base: '/my_readEra',
+});
