07bc6b95d03e69c220c0ed8999eee0a66ad13135
TimaSv-Uk <sviridenkotimofei@gmail.com>
2025-12-13 15:33:58 +0200

    fix github/workflows

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 
 export default defineConfig({
+  output: "static",
   site: "https://timasv-uk..github.io",
   base: '/my_readEra',
 });
